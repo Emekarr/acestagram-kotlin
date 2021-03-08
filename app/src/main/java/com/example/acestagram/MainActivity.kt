e3d57365cab86bf7c26c82178aa87f6469a71b87
emekarr <aceanyanwu02@gmail.com>
2021-03-08 22:54:14 +0100

    fiished the splash screen

--- app/src/main/java/com/example/acestagram/MainActivity.kt
+++ app/src/main/java/com/example/acestagram/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Switch from splash theme to original theme after app loads
+        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_main)
     }
 }
