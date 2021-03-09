6b095afcae8aa3bb0b4b76d1b15674a297916647
emekarr <aceanyanwu02@gmail.com>
2021-03-09 15:05:24 +0100

    added databinding to project. added layout tags to signup and login fragments. added navigation between these pages

--- app/src/main/java/com/example/acestagram/screens/loginscreen/LoginFragment.kt
+++ app/src/main/java/com/example/acestagram/screens/loginscreen/LoginFragment.kt
@@ -1,32 +1,36 @@
 package com.example.acestagram.screens.loginscreen
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.acestagram.R
+import com.example.acestagram.databinding.LoginFragmentBinding
 
 class LoginFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = LoginFragment()
-    }
-
+    private lateinit var binding: LoginFragmentBinding
     private lateinit var viewModel: LoginViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.login_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.login_fragment, container, false)
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
+        // navigate back to signup page
+        navigateToSignUpPage()
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun navigateToSignUpPage(){
+        binding.signUp.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+        }
     }
-
 }
